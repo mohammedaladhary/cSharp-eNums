5e60e00000af93762a9576fe20fc06fe50994905
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-05 13:15:55 +0400

    enum for gender and car type for user

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel;
+using System.Reflection;
+
+namespace enumPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("omar", 123, Gender.MALE, CarType.BASIC);
+            User user2 = new User("aisha", 120, Gender.FEMALE,CarType.PREMIUM);
+
+            Console.WriteLine((int)user1.carType);
+
+            //foreach(Gender gender in Enum.GetValues(typeof(Gender)))
+            //{
+            //    Console.WriteLine(gender.ToString());
+            //}
+
+            Console.WriteLine((int)Gender.FEMALE);
+            Console.WriteLine((int)CarType.VAN);
+            string description = getEnumDescription(CarType.VAN);
+            Console.WriteLine(description);
+
+
+            int ordinalValue = 200;
+            CarType type = (CarType)ordinalValue;
+            Console.WriteLine(type.ToString());
+
+        }
+
+        private static string getEnumDescription(Enum enums)
+        {
+            var field = enums.GetType().GetField(enums.ToString());
+            var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+            return attribute != null ? attribute.Description : enums.ToString();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumPractice
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public int id { get; set; }
+
+        public Gender gender { get; set; }
+
+        public CarType carType { get; set; }
+
+        public User(string Name, int id, Gender gender, CarType carType)
+        {
+            this.Name = Name;
+            this.id = id;
+            this.gender = gender;
+            this.carType = carType;
+        }
+    }
+
+    enum Gender
+    {
+        MALE,
+        FEMALE,
+        OTHER
+
+    }
+
+    enum CarType
+    {
+        [Description("this is BASIC")]
+        BASIC = 100,
+        [Description("this is PREMIUM ")]
+        PREMIUM = 200,
+        [Description("this is VAN ")]
+        VAN = 150,
+        [Description("this isULTRA_PREMIUM")]
+        ULTRA_PREMIUM = 250,
+
+
+    }
+}
